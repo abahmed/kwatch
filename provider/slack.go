8b789e667ca088017052d0e50fd55366c47882fd
Simon Frey <online.frey@mailbox.org>
2022-02-03 11:49:49 +0100

    Use slack blocks in slack handler (#63)
    * Add PodInitializing as ignored reason
    
    * Add "ignoreFailedGracefulShutdown" config parameter which allows to declare forced shutdowns not as error
    
    Containers which are forcefully killed during shutdown (as their graceful shutdown failed) are not reported as error
    
    * Ignore non-error exit codes 143 and 0 as we don't want to raise an error on them
    
    * Use slack blocks interface and split up messages to show full events and logs messages

--- provider/slack.go
+++ provider/slack.go
@@ -2,6 +2,7 @@ package provider
 
 import (
 	"errors"
+	"fmt"
 	"strings"
 
 	"github.com/abahmed/kwatch/event"
@@ -41,48 +42,6 @@ func (s *slack) SendEvent(ev *event.Event) error {
 		return errors.New("webhook url is empty")
 	}
 
-	// initialize fields with basic info
-	fields := []slackClient.AttachmentField{
-		{
-			Title: "Name",
-			Value: ev.Name,
-			Short: true,
-		},
-		{
-			Title: "Container",
-			Value: ev.Container,
-			Short: true,
-		},
-		{
-			Title: "Namespace",
-			Value: ev.Namespace,
-			Short: true,
-		},
-		{
-			Title: "Reason",
-			Value: ev.Reason,
-			Short: true,
-		},
-	}
-
-	// add events part if it exists
-	events := strings.TrimSpace(ev.Events)
-	if len(events) > 0 {
-		fields = append(fields, slackClient.AttachmentField{
-			Title: ":mag: Events",
-			Value: "```\n" + events + "```",
-		})
-	}
-
-	// add logs part if it exists
-	logs := strings.TrimSpace(ev.Logs)
-	if len(logs) > 0 {
-		fields = append(fields, slackClient.AttachmentField{
-			Title: ":memo: Logs",
-			Value: "```\n" + logs + "```",
-		})
-	}
-
 	// use custom title if it's provided, otherwise use default
 	title := viper.GetString("alert.slack.title")
 	if len(title) == 0 {
@@ -95,17 +54,46 @@ func (s *slack) SendEvent(ev *event.Event) error {
 		text = defaultText
 	}
 
-	msg := slackClient.WebhookMessage{
-		Attachments: []slackClient.Attachment{
-			{
-				Color:      "danger",
-				Title:      title,
-				Text:       text,
-				Fields:     fields,
-				MarkdownIn: []string{"fields"},
-				Footer:     footer,
+	blocks := []slackClient.Block{
+		markdownSection(title),
+		plainSection(text),
+		slackClient.SectionBlock{
+			Type: "section",
+			Fields: []*slackClient.TextBlockObject{
+				markdownF("*Name*\n%s", ev.Name),
+				markdownF("*Container*\n%s", ev.Container),
+				markdownF("*Namespace*\n%s", ev.Namespace),
+				markdownF("*Reason*\n%s", ev.Reason),
 			},
 		},
+	}
+
+	// add events part if it exists
+	events := strings.TrimSpace(ev.Events)
+	if len(events) > 0 {
+		blocks = append(blocks,
+			markdownSection(":mag: *Events*"))
+
+		for _, chunk := range chunks(events, 2000) {
+			blocks = append(blocks,
+				markdownSectionF("```%s```", chunk))
+		}
+	}
+
+	// add logs part if it exists
+	logs := strings.TrimSpace(ev.Logs)
+	if len(logs) > 0 {
+		blocks = append(blocks,
+			markdownSection(":memo: *Logs*"))
+
+		for _, chunk := range chunks(logs, 2000) {
+			blocks = append(blocks,
+				markdownSectionF("```%s```", chunk))
+		}
+	}
+
+	msg := slackClient.WebhookMessage{
+		Blocks: &slackClient.Blocks{BlockSet: append(blocks, markdownSection(footer))},
 	}
 
 	// send message
@@ -123,4 +111,54 @@ func (s *slack) SendMessage(msg string) error {
 		Text: msg,
 	}
 	return slackClient.PostWebhook(s.webhook, &sMsg)
+}
+
+func chunks(s string, chunkSize int) []string {
+	if len(s) == 0 {
+		return nil
+	}
+	if chunkSize >= len(s) {
+		return []string{s}
+	}
+	var chunks []string = make([]string, 0, (len(s)-1)/chunkSize+1)
+	currentLen := 0
+	currentStart := 0
+	for i := range s {
+		if currentLen == chunkSize {
+			chunks = append(chunks, s[currentStart:i])
+			currentLen = 0
+			currentStart = i
+		}
+		currentLen++
+	}
+	chunks = append(chunks, s[currentStart:])
+	return chunks
+}
+
+func plain(txt string) *slackClient.TextBlockObject {
+	return slackClient.NewTextBlockObject(slackClient.PlainTextType, txt, true, false)
+}
+func plainSection(txt string) slackClient.SectionBlock {
+	return slackClient.SectionBlock{
+		Type: "section",
+		Text: plain(txt),
+	}
+}
+func markdown(txt string) *slackClient.TextBlockObject {
+	return slackClient.NewTextBlockObject(slackClient.MarkdownType, txt, false, true)
+}
+func markdownSection(txt string) slackClient.SectionBlock {
+	return slackClient.SectionBlock{
+		Type: "section",
+		Text: markdown(txt),
+	}
+}
+func markdownF(format string, a ...interface{}) *slackClient.TextBlockObject {
+	return slackClient.NewTextBlockObject(slackClient.MarkdownType, fmt.Sprintf(format, a...), false, true)
+}
+func markdownSectionF(format string, a ...interface{}) slackClient.SectionBlock {
+	return slackClient.SectionBlock{
+		Type: "section",
+		Text: markdownF(format, a...),
+	}
 }
