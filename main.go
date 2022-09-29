b1325d53bbfc9b95d7739c6240c5d11475643d1e
Abdelrahman Ahmed <a.ahmed1026@gmail.com>
2022-09-29 16:59:54 +0200

    fix markdown issues in ms teams and rocketchat

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/abahmed/kwatch/config"
 	"github.com/abahmed/kwatch/constant"
 	"github.com/abahmed/kwatch/controller"
+	"github.com/abahmed/kwatch/event"
 	"github.com/abahmed/kwatch/upgrader"
 	"github.com/sirupsen/logrus"
 )
@@ -21,6 +22,16 @@ func main() {
 
 	alertManager := alertmanager.AlertManager{}
 	alertManager.Init(config.Alert)
+
+	alertManager.NotifyEvent(event.Event{
+		Name:      "jb6e58fc08-ea2b-46ea-bb87-6123a4e94bcc-d6hxz",
+		Container: "jb6e58fc08-ea2b-46ea-bb87-6123a4e94bcc",
+		Namespace: "production",
+		Reason:    "OOMKILLED",
+		Events:    "[2022-09-29 022042 +0000 UTC] SuccessfulAttachVolume AttachVolume.Attach succeeded for volume \"pvc-19096777-35bf-4d27-bf5f-ace40da5a49f\"\n[2022-09-29 022045 +0000 UTC] Created Created container jb6e58fc08-ea2b-46ea-bb87-6123a4e94bcc",
+		Logs:      "previous terminated container \"jb6e58fc08-ea2b-46ea-bb87-6123a4e94bcc\" in pod \"jb6e58fc08-ea2b-46ea-bb87-6123a4e94bcc-d6hxz\" not found",
+	})
+	return
 
 	// check and notify if newer versions are available
 	go upgrader.CheckUpdates(&config.Upgrader, &alertManager)
