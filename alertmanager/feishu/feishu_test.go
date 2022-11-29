3dce79f898d25cf0fb89d20f29dad24de1e4c802
Cong <heian0224@gmail.com>
2022-11-29 21:28:13 +0800

    add feishu webhook config for china users.

---
+++ alertmanager/feishu/feishu_test.go
@@ -0,0 +1,142 @@
+package feishu
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/abahmed/kwatch/event"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEmptyConfig(t *testing.T) {
+	assertions := assert.New(t)
+
+	c := NewFeiShu(map[string]string{})
+	assertions.Nil(c)
+}
+
+func TestRocketChat(t *testing.T) {
+	assertions := assert.New(t)
+
+	config := map[string]string{
+		"webhook": "testtest",
+	}
+	c := NewFeiShu(config)
+	assertions.NotNil(c)
+
+	assertions.Equal(c.Name(), "Fei Shu")
+}
+
+func TestBuildRequestBodyFeiShu(t *testing.T) {
+	assertions := assert.New(t)
+	s := httptest.NewServer(
+		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.Write([]byte(`{"isOk": true}`))
+		}))
+
+	defer s.Close()
+
+	config := map[string]string{
+		"webhook": s.URL,
+	}
+	c := NewFeiShu(config)
+	assertions.NotNil(c)
+	ev := event.Event{
+		Name:      "test-pod",
+		Container: "test-container",
+		Namespace: "default",
+		Reason:    "OOMKILLED",
+		Logs:      "test\ntestlogs",
+		Events:    "test",
+	}
+	expectMessage := "{\"msg_type\": \"post\",\"content\": {\"post\": {\"en_us\":  {\"title\":\"\",\"content\": [[" +
+		"{\"tag\":\"text\",\"text\":\"**Pod:** test-pod\\n**Container:** test-container\\n**Namespace:** default\\n**Reason:** OOMKILLED\\n**Events:**\\n```\\ntest\\n```\\n**Logs:**\\n```\\ntest\\ntestlogs\\n```\"}]]}}}}"
+	assertions.Equal(expectMessage, c.buildRequestBodyFeiShu(&ev, ""))
+}
+
+func TestSendMessage(t *testing.T) {
+	assertions := assert.New(t)
+
+	s := httptest.NewServer(
+		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.Write([]byte(`{"isOk": true}`))
+		}))
+
+	defer s.Close()
+
+	config := map[string]string{
+		"webhook": s.URL,
+	}
+	c := NewFeiShu(config)
+	assertions.NotNil(c)
+
+	assertions.Nil(c.SendMessage("test"))
+}
+
+func TestSendMessageError(t *testing.T) {
+	assertions := assert.New(t)
+
+	s := httptest.NewServer(
+		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusBadGateway)
+		}))
+
+	defer s.Close()
+
+	config := map[string]string{
+		"webhook": s.URL,
+	}
+	c := NewFeiShu(config)
+	assertions.NotNil(c)
+
+	assertions.NotNil(c.SendMessage("test"))
+}
+
+func TestSendEvent(t *testing.T) {
+	assertions := assert.New(t)
+
+	s := httptest.NewServer(
+		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.Write([]byte(`{"isOk": true}`))
+		}))
+
+	defer s.Close()
+
+	config := map[string]string{
+		"webhook": s.URL,
+	}
+	c := NewFeiShu(config)
+	assertions.NotNil(c)
+
+	ev := event.Event{
+		Name:      "test-pod",
+		Container: "test-container",
+		Namespace: "default",
+		Reason:    "OOMKILLED",
+		Logs:      "test\ntestlogs",
+		Events: "event1-event2-event3-event1-event2-event3-event1-event2-" +
+			"event3\nevent5\nevent6-event8-event11-event12",
+	}
+	assertions.Nil(c.SendEvent(&ev))
+}
+
+func TestInvalidHttpRequest(t *testing.T) {
+	assertions := assert.New(t)
+
+	config := map[string]string{
+		"webhook": "h ttp://localhost",
+	}
+	c := NewFeiShu(config)
+	assertions.NotNil(c)
+
+	assertions.NotNil(c.SendMessage("test"))
+
+	config = map[string]string{
+		"webhook": "http://localhost:132323",
+	}
+	c = NewFeiShu(config)
+	assertions.NotNil(c)
+
+	assertions.NotNil(c.SendMessage("test"))
+}
