9b2e425b347a57987a45445a34116af42592d4ca
Abdelrahman Ahmed <a.ahmed1026@gmail.com>
2024-02-29 20:43:00 +0200

    remove comments

--- filter/eventFilter.go
+++ filter/eventFilter.go
@@ -6,27 +6,7 @@ func (f EventFilter) Execute(ctx *Context) bool {
 	if ctx.EvType == "DELETED" {
 		ctx.Memory.DelPod(ctx.Pod.Namespace, ctx.Pod.Name)
 		return true
-		//ctx.PodHasIssues = true
 	}
-	/*
-		for _, c := range ctx.Pod.Status.ContainerStatuses {
-			if ctx.EvType == "MODIFIED" &&
-				ctx.Memory.HasPodContainer(
-					ctx.Pod.Namespace,
-					ctx.Pod.Name,
-					c.Name) {
-				continue
-			}
-
-			ctx.Memory.AddPodContainer(
-				ctx.Pod.Namespace,
-				ctx.Pod.Name,
-				c.Name,
-				&storage.ContainerState{
-					RestartCount: c.RestartCount,
-				})
-			// ctx.StoreUpdated[c.Name] = true
-		}*/
 
 	return false
 }
